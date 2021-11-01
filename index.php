189bd7d08e5de4d042a2088c4e93586f9b6d2cb9
PieroV <a_pvillanuevam@unjbg.edu.pe>
2021-10-31 19:16:43 -0500

    FAQ 2

--- index.php
+++ index.php
@@ -1,83 +1,88 @@
-<?php 
+<?php
 require_once("header.php");
-?> 
+?>
 
-  <!-- ======= Hero Section ======= -->
-  <section id="hero" class="d-flex align-items-center">
+<!-- ======= Hero Section ======= -->
+<section id="hero" class="d-flex align-items-center">
 
-    <div class="container" data-aos="zoom-out" data-aos-delay="100">
-      <div class="row">
-        <div class="col-xl-6">
-          <h1>DIRECCIÓN ACADÉMICA DE RESPONSABILIDAD SOCIAL UNIVERSITARIA</h1>
-          <a href="#about" class="btn-get-started scrollto">Ver noticias</a>
-        </div>
+  <div class="container" data-aos="zoom-out" data-aos-delay="100">
+    <div class="row">
+      <div class="col-xl-6">
+        <h1>DIRECCIÓN ACADÉMICA DE RESPONSABILIDAD SOCIAL UNIVERSITARIA</h1>
+        <a href="#about" class="btn-get-started scrollto">Ver noticias</a>
       </div>
     </div>
+  </div>
 
-  </section><!-- End Hero -->
+</section><!-- End Hero -->
 
 <?php
 require("logos.php");
 ?>
-    <!-- ======= PREGUNTAS FRECUENTES ======= -->
-    <section id="faq" class="faq">
-      <div class="container" data-aos="fade-up">
+<!-- ======= PREGUNTAS FRECUENTES ======= -->
+<section id="faq" class="faq">
+  <div class="container" data-aos="fade-up">
 
-        <div class="section-title">
-          <h2>PREGUNTAS FRECUENTES</h2>
+    <div class="section-title">
+      <h2>PREGUNTAS FRECUENTES</h2>
+    </div>
+
+    <ul class="faq-list accordion" data-aos="fade-up">
+
+      <li>
+        <a data-bs-toggle="collapse" class="collapsed" data-bs-target="#faq1">¿QUÉ ES LA DRSU? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-x icon-close"></i></a>
+        <div id="faq1" class="collapse" data-bs-parent=".faq-list">
+          <p>
+            Es una política de calidad ética del desempeño de la comunidad universitaria
+            (estudiantes, docentes y
+            personal
+            administrativo) a través de la gestión responsable de los impactos educativos,
+            cognitivos, laborales y
+            ambientales que
+            la Universidad genera, en un diálogo participativo con la sociedad para promover el
+            Desarrollo Humano
+            Sostenible.
+          </p>
         </div>
+      </li>
 
-        <ul class="faq-list accordion" data-aos="fade-up">
+      <li>
+        <a data-bs-toggle="collapse" data-bs-target="#faq2" class="collapsed">¿QUÉ ÁREAS INTEGRAN LA DRSU?<i class="bx bx-chevron-down icon-show"></i><i class="bx bx-x icon-close"></i></a>
+        <div id="faq2" class="collapse" data-bs-parent=".faq-list">
+          <p>
+            Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id
+            donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit
+            ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.
+          </p>
+        </div>
+      </li>
 
-          <li>
-            <a data-bs-toggle="collapse" class="collapsed" data-bs-target="#faq1">¿QUÉ ES LA DRSU? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-x icon-close"></i></a>
-            <div id="faq1" class="collapse" data-bs-parent=".faq-list">
-              <p>
-                Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet non curabitur
-                gravida. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non.
-              </p>
-            </div>
-          </li>
+      <li>
+        <a data-bs-toggle="collapse" data-bs-target="#faq3" class="collapsed">¿ES IMPORTANTE LA RESPONSABILIDAD SOCIAL UNIVERSITARIA?<i class="bx bx-chevron-down icon-show"></i><i class="bx bx-x icon-close"></i></a>
+        <div id="faq3" class="collapse" data-bs-parent=".faq-list">
+          <p>
+            Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Faucibus pulvinar elementum
+            integer enim. Sem nulla pharetra diam sit amet nisl suscipit. Rutrum tellus pellentesque eu tincidunt.
+            Lectus urna duis convallis convallis tellus. Urna molestie at elementum eu facilisis sed odio morbi quis
+          </p>
+        </div>
+      </li>
 
-          <li>
-            <a data-bs-toggle="collapse" data-bs-target="#faq2" class="collapsed">¿QUÉ ÁREAS INTEGRAN LA DRSU?<i class="bx bx-chevron-down icon-show"></i><i
-                class="bx bx-x icon-close"></i></a>
-            <div id="faq2" class="collapse" data-bs-parent=".faq-list">
-              <p>
-                Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id
-                donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit
-                ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.
-              </p>
-            </div>
-          </li>
+      <li>
+        <a data-bs-toggle="collapse" data-bs-target="#faq4" class="collapsed">¿DÓNDE SE ENCUENTRA LA DRSU EN LA UNJBG?<i class="bx bx-chevron-down icon-show"></i><i class="bx bx-x icon-close"></i></a>
+        <div id="faq4" class="collapse" data-bs-parent=".faq-list">
+          <p>
+            Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id
+            donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit
+            ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.
+          </p>
+        </div>
+      </li>
 
-          <li>
-            <a data-bs-toggle="collapse" data-bs-target="#faq3" class="collapsed">¿ES IMPORTANTE LA RESPONSABILIDAD SOCIAL UNIVERSITARIA?<i class="bx bx-chevron-down icon-show"></i><i
-                class="bx bx-x icon-close"></i></a>
-            <div id="faq3" class="collapse" data-bs-parent=".faq-list">
-              <p>
-                Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Faucibus pulvinar elementum
-                integer enim. Sem nulla pharetra diam sit amet nisl suscipit. Rutrum tellus pellentesque eu tincidunt.
-                Lectus urna duis convallis convallis tellus. Urna molestie at elementum eu facilisis sed odio morbi quis
-              </p>
-            </div>
-          </li>
+    </ul>
 
-          <li>
-            <a data-bs-toggle="collapse" data-bs-target="#faq4" class="collapsed">¿DÓNDE SE ENCUENTRA LA DRSU EN LA UNJBG?<i class="bx bx-chevron-down icon-show"></i><i class="bx bx-x icon-close"></i></a>
-            <div id="faq4" class="collapse" data-bs-parent=".faq-list">
-              <p>
-                Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id
-                donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit
-                ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.
-              </p>
-            </div>
-          </li>
-
-        </ul>
-
-      </div>
-    </section><!-- End PREGUNTAS FRECUENTES -->
+  </div>
+</section><!-- End PREGUNTAS FRECUENTES -->
 
 <?php
 require("footer.php");
