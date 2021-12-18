d4bea43e82df1780bb8a505b9be65f970689bf29
brafer99 <brayanfernando02.99@gmail.com>
2021-12-17 22:23:41 -0500

    modifiacion de index

--- public/header.php
+++ public/header.php
@@ -41,7 +41,7 @@
     <!-- ======= Header ======= -->
     <header id="header" class="fixed-top d-flex align-items-center">
         <div class="container d-flex align-items-center">
-            <h1 class="logo me-auto"><a href="../index.php"><img src="../assets/img/rrsu.png" alt="logo">  DRSU</a></h1>
+            <h1 class="logo me-auto"><a href="../index.php"><img src="../assets/img/rsU2.png" alt="logo">  DRSU</a></h1>
             <!-- Uncomment below if you prefer to use an image logo -->
             <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt=""></a>-->
             <nav id="navbar" class="navbar order-last order-lg-0">
@@ -69,11 +69,11 @@
                             break;
                     }
                     echo '
-                    <li><a class="nav-link scrollto '.$url1.'" href="../index.php">Inicio</a></li>
-                    <li><a class="nav-link scrollto '.$url2.'" href="nosotros.php">Nosotros</a></li>
-                    <li><a class="nav-link scrollto '.$url5.'" href="autoridades.php">Autoridades</a></li>
-                    <li><a class="nav-link scrollto '.$url3.'" href="noticias.php">Noticias</a></li>
-                    <li><a class="nav-link scrollto '.$url4.'" href="contacto.php">Contáctanos</a></li>
+                    <li><a class="nav-link scrollto '.$url1.'" href="../index.php">INICIO</a></li>
+                    <li><a class="nav-link scrollto '.$url2.'" href="nosotros.php">NOSOTROS</a></li>
+                    <li><a class="nav-link scrollto '.$url5.'" href="autoridades.php">AUTORIDADES</a></li>
+                    <li><a class="nav-link scrollto '.$url3.'" href="noticias.php">NOTICIAS</a></li>
+                    <li><a class="nav-link scrollto '.$url4.'" href="contacto.php">CONTÁCTANOS</a></li>
                     ';?>
                 </ul>
                 <i class="bi bi-list mobile-nav-toggle"></i>
